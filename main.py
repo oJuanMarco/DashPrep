60c9840e480d0078564246c60d7183a1c1534a80
Juan <juanbusine22@gmail.com>
2026-08-21 08:03:37 -0300

    Adding docstrings and folder and logic organization

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 # import do pandas para manipulação de dados, da função e variaveis pro funcionamento do código
 import pandas as pd
-from read import read_file
-from tables import clientes, contas, emprestimos, transacoes
+from functions.read import read_file
+from variables.tables import clientes, contas, emprestimos, transacoes
 
+# lista com tabelas para identação no for e variavel para localizar qual a tabela referente
 tabelas = [clientes,contas,emprestimos,transacoes]
+var = 0
 
 # print(tabela1+"oi") == Se eu somar a leitura à um elemento, todos os elementos da tabela
 # serão afetados
-var = 0
+
+# laço passa por cada tabela da lista
 for tabela in tabelas:
+    # printa de qual tabela os dados correspondem
     print(f"Dados da tabela {str(tabelas[var]).capitalize()}:")
+    # leitura dos dados da tabela pelo pandas
     dados = read_file(tabela)
+    # localização da coluna: nome, tipo de dados e exemplo de entrada
     for colunm in dados.columns:
-        # print(dados[colunm].head(1))
         print(f"{colunm} = {dados[colunm].dtype}")
         print(f"Exemplo de dado: {dados[colunm].iloc[0]}")
     print("\n")
+    # soma de variavel pra passagem da proxima tabela
     var+=1
