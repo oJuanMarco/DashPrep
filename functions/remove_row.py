3f3986f8ebc2d2847c1853a731c9ba9e31fddee0
Juan <juanbusine22@gmail.com>
2026-08-24 05:40:06 -0300

    Fixing my mistake that excludes remove char function

--- functions/remove_row.py
+++
@@ -1,9 +0,0 @@
-# função que remove linha se tiver dado vazio ou periodo de transações > 12
-# adiciona contador pra analizar quantas linhas foram removidas
-
-def row_remove(query):
-    
-    soma = query.isna().any(axis=1).sum()
-        
-    print(f"{soma} linhas deletadas de {len(query)} linhas")
-    input(f"Cera de {(soma/len(query))*100:.2f}%")
--- functions/treatment.py
+++ functions/treatment.py
@@ -1,7 +1,8 @@
-# esta função é a principal na parte de tratamento, chama todas as ordem na ordem estipulada para tratamento manual preciso
+# esta função é a principal na parte de tratamento, chama todas as ordem na ordem estipulada para tratamento manual precisa
 from functions.blank_space_remove import blank_space_remove
 from functions.change_column_name import change_column_name
 from functions.change_char import char_change
+from functions.remove_char import char_remove
 from functions.change_type import change_column_type
 from functions.clear_screen import clear_screen
 
